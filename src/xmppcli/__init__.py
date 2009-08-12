bac579175b9b6afabf2398afa7088085a3cf9675
baron <baron@localhost>
2009-08-12 14:26:25 +0000

    r324@gamer:  baron | 2009-08-11 14:19:03 -0600  added rows() to TextBuffer for mouse clicking.  Fixed up licenses

--- src/xmppcli/__init__.py
+++ src/xmppcli/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2008 James Newton
+# Copyright (C) 2009 James Newton
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -13,8 +13,6 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-
-
 
 import sys, xmpp
 #import xmppparser
